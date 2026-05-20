6b817c8ee1c1bf5b66552e86ac420ecda88758f7
Huỳnh Mai Nhật Minh <156114356+huynhmainhatminh@users.noreply.github.com>
2026-05-20 17:29:22 +0700

    update basic base

---
+++ src/main/java/com/team4tech/evbatteryswap/entity/Station.java
@@ -0,0 +1,43 @@
+package com.team4tech.evbatteryswap.entity;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.annotations.ColumnDefault;
+
+import java.time.Instant;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "stations")
+public class Station {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Integer id;
+
+    @Size(max = 255)
+    @NotNull
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    @NotNull
+    @Column(name = "address", nullable = false, length = Integer.MAX_VALUE)
+    private String address;
+
+    @NotNull
+    @Column(name = "status", nullable = false, length = Integer.MAX_VALUE)
+    private String status;
+
+    @ColumnDefault("CURRENT_TIMESTAMP")
+    @Column(name = "created_at")
+    private Instant createdAt;
+
+    @ColumnDefault("CURRENT_TIMESTAMP")
+    @Column(name = "updated_at")
+    private Instant updatedAt;
+
+}
--- src/main/java/com/team4tech/evbatteryswap/entity/User.java
+++ src/main/java/com/team4tech/evbatteryswap/entity/User.java
@@ -1,4 +1,68 @@
-package com.team4tech.evbatteryswap.entity;
+package com.team4tech.evbatteryswap;
 
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.annotations.ColumnDefault;
+
+import java.math.BigDecimal;
+import java.time.Instant;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "users")
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Integer id;
+
+    @Size(max = 255)
+    @NotNull
+    @Column(name = "username", nullable = false)
+    private String username;
+
+    @Size(max = 255)
+    @NotNull
+    @Column(name = "full_name", nullable = false)
+    private String fullName;
+
+    @Size(max = 255)
+    @ColumnDefault("NULL")
+    @Column(name = "email")
+    private String email;
+
+    @Size(max = 20)
+    @ColumnDefault("NULL")
+    @Column(name = "phone_number", length = 20)
+    private String phoneNumber;
+
+    @Size(max = 255)
+    @NotNull
+    @Column(name = "password", nullable = false)
+    private String password;
+
+    @ColumnDefault("0.00")
+    @Column(name = "wallet_balance", precision = 12, scale = 2)
+    private BigDecimal walletBalance;
+
+    @Size(max = 255)
+    @ColumnDefault("'DRIVER'")
+    @Column(name = "role")
+    private String role;
+
+    @Column(name = "status", length = Integer.MAX_VALUE)
+    private String status;
+
+    @ColumnDefault("CURRENT_TIMESTAMP")
+    @Column(name = "created_at")
+    private Instant createdAt;
+
+    @ColumnDefault("CURRENT_TIMESTAMP")
+    @Column(name = "updated_at")
+    private Instant updatedAt;
+
 }
