e806af59204ef8aa82fae9a69f6cdd5c852496db
Huỳnh Mai Nhật Minh <156114356+huynhmainhatminh@users.noreply.github.com>
2026-05-21 22:56:16 +0700

    Update CrossOrigin

--- src/main/java/com/team4tech/evbatteryswap/controller/AuthController.java
+++ src/main/java/com/team4tech/evbatteryswap/controller/AuthController.java
@@ -16,6 +16,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/auth")
+@CrossOrigin(origins = "http://localhost:5173")
 @Tag(name = "Authentication", description = "Login to obtain a JWT token")
 public class AuthController {
 
