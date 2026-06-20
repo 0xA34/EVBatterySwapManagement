967ab2700598939fbf9600fb336243a6049328fa
Huỳnh Mai Nhật Minh <156114356+huynhmainhatminh@users.noreply.github.com>
2026-06-20 23:43:00 +0700

    update CrossOrigin StationHomeController

--- src/main/java/com/team4tech/evbatteryswap/controller/StationHomeController.java
+++ src/main/java/com/team4tech/evbatteryswap/controller/StationHomeController.java
@@ -7,14 +7,12 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/station")
+@CrossOrigin(origins = "http://localhost:5173")
 public class StationHomeController {
 
     private final StationService stationService;
